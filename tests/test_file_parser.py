08114473a841319002ffe60e4aeab0c1f2c16bfe
perocha <pedrojrocha@outlook.com>
2023-03-07 16:59:26 +0100

    Update unit tests for file parser

--- tests/test_file_parser.py
+++ tests/test_file_parser.py
@@ -13,3 +13,9 @@ def test_file_parser_init():
     assert file_parser.file_path == "test_file.xlsx"
     assert file_parser.rows == []
     assert file_parser.headers == []
+
+def test_read_excel_file():
+    file_parser = FileParser("test_file.xlsx")
+    file_parser.read_excel_file()
+    assert len(file_parser.rows) > 0
+    assert len(file_parser.headers) > 0
